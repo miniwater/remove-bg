60b674e7b5adff5f5faf72c6cb486d84de3e0749
MiniWater <1456441157@qq.com>
2026-01-17 16:39:27 +0800

    fallback

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,39 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  // async headers() {
-    // return [
-    //   {
-    //     source: "/api/:path*",
-    //     headers: [
-    //       { key: "Access-Control-Allow-Credentials", value: "true" },
-    //       { key: "Access-Control-Allow-Origin", value: "*" },
-    //       {
-    //         key: "Access-Control-Allow-Methods",
-    //         value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-    //       },
-    //       {
-    //         key: "Access-Control-Allow-Headers",
-    //         value:
-    //           "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     source: "/(.*)",
-    //     headers: [
-    //       {
-    //         key: "Cross-Origin-Opener-Policy",
-    //         value: "same-origin",
-    //       },
-    //       {
-    //         key: "Cross-Origin-Embedder-Policy",
-    //         value: "require-corp",
-    //       },
-    //     ],
-    //   },
-    // ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
